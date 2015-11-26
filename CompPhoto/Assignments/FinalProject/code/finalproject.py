dd5ccf019c62aa985397b79933eb23df477f31e2
jkilver <robobookworm@gmail.com>
2015-11-26 08:27:49 -0500

    CP final project progress

--- CompPhoto/Assignments/FinalProject/code/finalproject.py
+++ CompPhoto/Assignments/FinalProject/code/finalproject.py
@@ -2,28 +2,67 @@ import numpy as np
 import cv2
 
 import os
+import sys
 
-
-
+def moving_avg(arr, window_size):
+    # Average along the depths of the array (i.e, along the time axis)
+    time_cumsum = np.cumsum(arr, axis=2, dtype=float)
+    
+    time_cumsum[:,:,window_size:] = time_cumsum[:,:,window_size:] - time_cumsum[:,:,:-window_size]
+    
+    return time_cumsum[:,:,window_size-1:] / window_size
+#end moving_avg
 
 
 
 def main:
-    # Ask for video to stabilize from user
+    # Get video to stabilize from the commandn line
+    filename = sys.argv[1]
+    
+    # Open video
+    video = cv2.VideoCapture(filename)
+    # Get the number of frames
+    numFrames = video.get(CV_CAP_PROP_FRAME_COUNT)
+    
+    # Take the first frame and determine good features to track
+    retval, frame1 = video.read()
     
     # Determine good points to track
-    cv2.goodFeaturesToTrack()
+    maxNumCorners = 200
+    qualityLevel = 1
+    minDistance = 10
+    prevPts = cv2.goodFeaturesToTrack(ff, maxNumCorners, qualityLevel, minDistance )
     
+    # While there are still frames, iterate over the frames
+    currIdx = 0
+    transforms = np.zeros( (2, 3, numFrames-1) )
+    retval, frame2 = video.read()
+    while retval:
+        nextPts, status, err = cv2.calcOpticalFlowPyrLK(frame1, frame2, prevPts, flags=OPTFLOW_USE_INITIAL_FLOW)
+        
+        # Here I think I can compute the flow by taking the difference between the prevPts and nextPts vector
+        # However, this would only be for illustrative purposes. It is not necessary for computing the transformation
+        # matrix
+        
+        # Determine transformation from flow
+        # Just use a rigid transform (translation, rotation, uniform scale)
+        transformMatrix = cv2.estimateRigidTransform(prevPts, nextPts, fullAffine=false)
+        
+        # Add transformation matrix to list
+        transforms[:,:,currIdx] = transformMatrix
+        
+        # Swap out frames and points
+        nextPts = prevPts
+        frame1 = frame2
+        retval, frame2 = video.read()
+        currIdx += 1
+    #end while
     
-    # Determine flow
-    cv2.calcOpticalFlowLK()
+    # Here would be a good place to save off the transform matrices so we can plot the paths
     
-    # Determine transformation from flow
-    # For now this will just be translation and rotation (rigid transform)
-    cv2.estimateRigidTransform()
+    # Now that we have all the transformations, use a moving average window to calculate the
+    # better camera path
     
-    # Accumulate transforms and smooth to determine new path
-    # Use moving average window
     
     # Compute new transforms using new path
     
