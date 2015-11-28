0757d41029f70d910d3f0fb718f726d1ead05f80
jkilver <robobookworm@gmail.com>
2015-11-28 16:38:43 -0500

    CP Final project working

--- CompPhoto/Assignments/FinalProject/code/finalproject.py
+++ CompPhoto/Assignments/FinalProject/code/finalproject.py
@@ -4,17 +4,6 @@ import cv2
 import os
 import sys
 
-def moving_avg_poor(vector, window_size):
-    returnVal = np.zeros(vector.shape)
-    returnVal[0:window_size] = vector[0:window_size]
-
-    time_cumsum = np.cumsum(vector, dtype=float)
-    
-    time_cumsum[window_size:] = time_cumsum[window_size:] - time_cumsum[:-window_size]
-    
-    return time_cumsum[window_size-1:] / window_size
-#end moving_avg
-
 def moving_avg(vector, window_size):
     '''
     Apply a moving average filter in both directions (i.e., filter window is centered)
@@ -51,29 +40,6 @@ def moving_avg(vector, window_size):
     
     return avg_vec
 #def moving_avg
-
-
-def moving_avg_3d(arr, window_size):
-    '''
-    Input: arr - a 3 dimensional array
-           window_size: the window over which you wish to use the moving average filter
-    
-    '''
-    # I had trouble getting the multidimensional moving average filter working, so
-    # I am just going to break the array into vectors and feed into the 1D filter
-    
-    averaged = np.zeros( arr.shape )
-    
-    for i in range(arr.shape[0]):
-        for j in range(arr.shape[1]):
-            # Take the time vector of the row and column and pass to 
-            # the 1D moving average filter
-            curr_vec = arr[i, j, :]
-            avg_vec = moving_avg(curr_vec, window_size)
-            averaged[i,j,:] = avg_vec
-    #end for
-    return averaged
-#ennd moving_avg_3d
 
 
 def main():
@@ -135,8 +101,9 @@ def main():
         # matrix
         
         # Determine transformation from flow
-        # Just use a rigid transform (translation, rotation, uniform scale)
+        # Just use a rigid transform (translation, rotation)
         transformMatrix = cv2.estimateRigidTransform(prevPts, nextPts, fullAffine=False)
+        # For future work, try doing a full homography
         
         # Add transformation matrix to list
         transforms[:,:,currIdx] = transformMatrix
@@ -160,8 +127,6 @@ def main():
     # At this point, we have the sequence of matrices that have the deltas between each frame
     # However, I need the actual path. 
     # Use the pairwise differences to build the actual camera path
-    # TODO: CHECK THIS!
-    # I think this is right
     dx = transforms[0,2,:]
     dy = transforms[1,2,:]
     da = np.arctan2(transforms[1,0,:], transforms[0,0,:])
@@ -228,28 +193,42 @@ def main():
     #********************************************
     # Apply new transforms to video
     #********************************************
-    # Return video reading to the beginning
-    video.set(cv2.CV_CAP_PROP_POS_FRAMES, 0)
+    crop_size = 30
+    fourcc = cv2.cv.CV_FOURCC('D','I','V','X')
+    video_out = cv2.VideoWriter('output.avi', 
+                                 fourcc,
+                                 30,
+                                 (frame1.shape[1]-2*crop_size, frame1.shape[0]-2*crop_size)    )
     
-    for i in range(numFrames-1):
+    # Return video reading to the beginning
+    video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+    for i in range(int(numFrames)-1):
+        # Get the frame and make sure it is valid
         retval, frame = video.read()
-        if retval == false:
+        if retval == False:
             break;
         
+        # Construct the transform
         transform_temp = np.zeros( (2, 3) )
-        transform_temp[0,0] =  cos(da[i])
-        transform_temp[0,1] = -sin(da[i])
-        transform_temp[1,0] =  sin(da[i])
-        transform_temp[1,1] =  cos(da[i])
+        transform_temp[0,0] =  np.cos(da[i])
+        transform_temp[0,1] = -np.sin(da[i])
+        transform_temp[1,0] =  np.sin(da[i])
+        transform_temp[1,1] =  np.cos(da[i])
         
         transform_temp[0,2] = new_dx[i]
         transform_temp[1,2] = new_dy[i]
         
-        frame_warp = cv2.warpAffine(frame, transform_temp, frame.shape)
+        # Apply the transform
+        frame_warp = cv2.warpAffine(frame, transform_temp, dsize=(frame.shape[1], frame.shape[0]))
         
-        cv2.imwrite( '..\\video\\frame' + i + '.png', frame_warp)
+        # Crop frame 
+        frame_warp = frame_warp[crop_size:frame_warp.shape[0]-crop_size, crop_size:frame_warp.shape[1]-crop_size]
         
+        #cv2.imwrite( '..\\video\\output\\frame' + str(i) + '.png', frame_warp)
+        video_out.write(frame_warp)
     #end for
+    video.release()
+    video_out.release()
     
 #end main
 
