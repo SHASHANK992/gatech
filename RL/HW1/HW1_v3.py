cd20465ed29343044f8b506634c87372ad4700ac
jkilver <robobookworm@gmail.com>
2016-01-23 09:57:07 -0500

    RL HW1 getting close

--- RL/HW1/HW1_v3.py
+++ RL/HW1/HW1_v3.py
@@ -18,34 +18,33 @@ def DieN_expectedValue(N=30, B=[0,1,1,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,
         if i not in B_list:
             A.append(i)
     #endfor
-        
-    # With your current stakes, calculate value of rolling again
-    ev = (1.0/N)*(sum(A) - len(B)*stake)
-    print ev
     
-    # If this value is positive, then I want to find the path that gives me
-    # maximum reward
-    if ev >= 0:
-        ev_max = 0
+    # With your current stakes, calculate value of rolling again
+    ev_init = (1.0/N)*(sum(A) - len(B_list)*stake)
+    ev = 0 
+    # If this value is positive, roll again!
+    if ev_init > 0:
         for val in A:
-            print "Recursion: ", val
+            print "Roll again! Stake =  ", val+stake
             ev_temp = DieN_expectedValue(N, B, val+stake)
             
-            if ev_temp > ev_max:
-                ev_max = ev_temp
+            if ev_temp < stake:
+                ev += float(val+stake)/N
         #end for
-        
-        ev = ev_max/len(B)
-        
     # Otherwise, I want to quit the game and return my current winnings
     else:
-        print "Returning: ", stake
-        ev = stake
+        print "Quitting. Winnings = ", stake
+        ev = stake/float(N)
     #end if
     
     return ev
 
 if __name__ == "__main__":
-    ev = DieN_expectedValue(N=8, B=[1,0,1,0,1,1,1,0])
     
+    ev = DieN_expectedValue(N=8, B=[1,0,1,0,1,1,1,0])
     print "Expected Value: ", ev
+    
+    '''
+    ev = DieN_expectedValue(N=4, B=[0,1,1,1])
+    print "Expected Value: ", ev
+    '''
