2dcefc90c3bd749aa07bd124e7130abab315b6f9
jkilver <robobookworm@gmail.com>
2016-01-24 09:52:43 -0500

    RL HW1 final submission

--- RL/HW1/HW1.py
+++ RL/HW1/HW1.py
@@ -1,38 +1,58 @@
-'''
-A policy maps states to actions. I am taking the current state of this MDP to simply be the sum of 
-all rewards that have been accumulated so far. 
-
-With the MDP assumption, all I need to know to generate a policy is the current state.
 
 '''
-
-def DieN_policy( sum_of_rewards, B = [1,4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30], N=30):
-    # Build a list of the possible rewards
+Input:
+    N: Number of sides to the die
+    B: A list of of length N that has a 1 to note "bad" sides
+'''
+def DieN_expectedValue(N=30, B=[0,1,1,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0], stake=0):
+    # Set some things up first
+    
+    B_list = []
+    for i in range(1,N+1):
+        if B[i-1] == 1:
+            B_list.append(i)
+    #end for
+    
     A = []
     for i in range(1,N+1):
-        if i not in B:
+        if i not in B_list:
             A.append(i)
     #endfor
     
-    # This is the negative reward for rolling a number in B
-    regret = -sum_of_rewards
-    
-    # To determine whether to take the risk to roll again and lose all the accumulated
-    # reward, I need to do an expectation
-    # For regret, that is the probability that I roll a number in B * the amount of regret
-    EV_regret = (float(len(B))/N)*regret
-    
-    # For reward, is is just the probability that I roll a number not in B times the reward itself
-    EV_reward = (1.0/N)*sum(A)
-    
-    ret_val = ""
-    if abs(EV_regret) >= EV_reward:
-        ret_val = "Don't roll again. Quit."
+    # With your current stakes, calculate value of rolling again
+    ev_state = (1.0/N)*(sum(A) - len(B_list)*stake)
+    ev = 0 
+    # If this value is positive, roll again!
+    if ev_state > 0:
+        for val in A:
+            #print "Roll again! Stake =  ", val+stake
+            ev_temp = DieN_expectedValue(N, B, val+stake)
+            
+            ev += ev_temp/float(N)
+        #end for
+    # Otherwise, I want to quit the game and return my current winnings
     else:
-        ret_val = "Roll. The expected value of the reward is greater than the expected value of regret."
-        
-    return ret_val
-
+        #print "Quitting. Winnings = ", stake
+        ev = stake
+    #end if
+    
+    return ev
 
 if __name__ == "__main__":
-    print DieN_policy(5)
+    '''
+    ev = DieN_expectedValue(N=8, B=[1,0,1,0,1,1,1,0])
+    print "Expected Value: ", ev
+    
+    
+    ev = DieN_expectedValue(N=4, B=[0,1,1,1])
+    print "Expected Value: ", ev
+    '''
+    ev = DieN_expectedValue(N=5, B=[1,0,0,0,1])
+    print "EV1 = ", ev
+    
+    ev = DieN_expectedValue(N=19, B=[1,0,1,1,1,1,1,0,1,1,0,1,0,0,1,0,1,0,0])
+    print "EV2 = ", ev
+    
+    ev = DieN_expectedValue(N=4, B=[1,0,1,1])
+    print "EV3 = ", ev
+    
