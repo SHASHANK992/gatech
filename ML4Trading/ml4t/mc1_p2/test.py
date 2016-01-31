45b10c24672670be96de6faf5395cc1cb6a73105
jkilver <robobookworm@gmail.com>
2016-01-31 14:10:20 -0500

    ML4T MC1P2 final submission + Homework problem statements for RL and ML4T

--- ML4Trading/ml4t/mc1_p2/test.py
+++ ML4Trading/ml4t/mc1_p2/test.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # Define input parameters
     # Note that ALL of these values will be set to different values by
     # the autograder!
-    
+    '''
     print '********************************************'
     print 'Test 1'
     print '********************************************'
@@ -113,6 +113,29 @@ if __name__ == "__main__":
     start_date = dt.datetime(2010,1,1)
     end_date = dt.datetime(2010,12,31)
     symbols = ['GOOG', 'AAPL', 'GLD', 'HNZ']
+
+    # Assess the portfolio
+    allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
+        syms = symbols, \
+        gen_plot = True)
+
+    # Print statistics
+    print "Start Date:", start_date
+    print "End Date:", end_date
+    print "Symbols:", symbols
+    print "Allocations:", allocations
+    print "Sharpe Ratio:", sr
+    print "Volatility (stdev of daily returns):", sddr
+    print "Average Daily Return:", adr
+    print "Cumulative Return:", cr
+    '''
+    print '********************************************'
+    print 'Homework chart'
+    print '********************************************'
+
+    start_date = dt.datetime(2009,1,1)
+    end_date = dt.datetime(2010,12,31)
+    symbols = ['IBM', 'AAPL', 'HNZ', 'XOM', 'GLD']
 
     # Assess the portfolio
     allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
