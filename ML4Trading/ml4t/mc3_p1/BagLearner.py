7f7e9c3f91574d56c0f8558acde835740fd2a77e
Jacob Kilver <robobookworm@gmail.com>
2016-03-23 06:44:36 -0400

    ML4T Check-in for MC3 Projects 1, 2, and 3

---
+++ ML4Trading/ml4t/mc3_p1/BagLearner.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class BagLearner(object):
+
+	def __init__(self, learner, **kwargs, bags, boost):
+		pass
+		
+	def addEvidence(self,dataX,dataY):
+		""" To do"""
+		
+	def query(self,points):
+		""" TO DO """
---
+++ ML4Trading/ml4t/mc3_p1/KNNLearner.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class KNNLearner(object):
+
+	def __init__(self, k):
+		pass
+		
+	def addEvidence(self,dataX,dataY):
+		""" To do"""
+		
+	def query(self,points):
+		""" TO DO """
