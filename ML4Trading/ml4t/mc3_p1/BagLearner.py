6c107d0a79e35110c72c66b5c35a002e2eea2b3a
Jacob Kilver <robobookworm@gmail.com>
2016-03-24 21:57:05 -0400

    ML4T MC3 P1 Code working
    Need to complete report and code required for report

--- ML4Trading/ml4t/mc3_p1/BagLearner.py
+++ ML4Trading/ml4t/mc3_p1/BagLearner.py
@@ -1,12 +1,56 @@
 import numpy as np
+import KNNLearner as knnl
 
 class BagLearner(object):
 
-	def __init__(self, learner, **kwargs, bags, boost):
-		pass
+    def __init__(self, learner, kwargs, bags, boost ):
+        # Instantiate the learners
+        self.learners = []
+        self.bags = bags
+        self.boost = boost
+        for i in range(0,bags):
+            self.learners.append(learner(**kwargs))
+        #end for
 		
-	def addEvidence(self,dataX,dataY):
-		""" To do"""
 		
-	def query(self,points):
-		""" TO DO """
+    def addEvidence(self,dataX,dataY):
+        """ 
+        Each learner will be trained with dataX.shape[0]
+        data points. However, we sample with replacement,
+        so some data points will be repeated.
+        """
+        n = dataX.shape[0]
+        
+        # For each learner
+        for i in range(0,self.bags):
+            # Select n data points from dataX (with replacement)
+            # Along with their corresponding Y values
+            
+            # Generate indices of n samples randomly
+            indices = np.random.randint(n, size=n)
+            
+            # Sample data
+            X = np.zeros(dataX.shape)
+            Y = np.zeros(n)
+            for j in range(0,n):
+                X[j,:] = dataX[ indices[j], :]
+                Y[j]   = dataY[ indices[j] ]
+            #end for
+            
+            self.learners[i].addEvidence(X, Y)
+        #end for
+        
+		
+    def query(self,points):
+        """ 
+        """
+        Yout = np.zeros((points.shape[0]))
+        
+        # For each learner
+        for j in range(0,self.bags):
+            # Get the output from that learner
+            # Add to running sum
+            Yout += self.learners[j].query(points)
+        #end for
+        
+        return Yout/self.bags       
--- ML4Trading/ml4t/mc3_p1/KNNLearner.py
+++ ML4Trading/ml4t/mc3_p1/KNNLearner.py
@@ -2,36 +2,49 @@ import numpy as np
 
 class KNNLearner(object):
 
-	def __init__(self, k):
-		self.k = k
-		
-	def addEvidence(self,dataX,dataY):
-		""" 
-		From the example provided by the instructor, I don't think
-		that this function accumulates data. It is called once and the
-		training is complete. If this is called again, the previous training
-		is forgotten
-		
-		This is pretty easy to implement
-		"""
-		# Training is as easy as saving the data points for 
-		# querying later
-		self.Xdata = dataX
-		self.Ydata = dataY
-		
-	def query(self,points):
-		""" TO DO """
-		# Array to store output
-		Youtput = np.zeros(points.shape[0])
-		# For each query point
-		for i in range(0:points.shape[0]):
-			# Store distance with index distance corresponds to
-			distance = np.zeros(self.Xdata.shape[0], 2)
-			
-			for j in range(0:self.Xdata.shape[0]):
-				distance[j,0] = j
-				distance[j,1] = np.linalg.norm(points[i,:] - self.Xdata[j,:])
-			#end for
-			
-			# Sort distance data by second column
-			
+    def __init__(self, k):
+        self.k = k
+        
+    def addEvidence(self,dataX,dataY):
+        """ 
+        From the example provided by the instructor, I don't think
+        that this function accumulates data. It is called once and the
+        training is complete. If this is called again, the previous training
+        is forgotten
+        
+        This is pretty easy to implement
+        """
+        # Training is as easy as saving the data points for 
+        # querying later
+        self.Xdata = dataX
+        self.Ydata = dataY
+        
+    def query(self,points):
+        """ TO DO """
+        # Array to store output
+        Yout = np.zeros(points.shape[0])
+        # For each query point
+        for i in range(0,points.shape[0]):
+            # Store distance with index distance corresponds to
+            dtype = [('idx', int), ('dist', float)]
+            distance = np.zeros((self.Xdata.shape[0]), dtype=dtype)
+            
+            for j in range(0,self.Xdata.shape[0]):
+                distance[j] = ( j, np.linalg.norm(points[i,:] - self.Xdata[j,:]) )
+            #end for
+            
+            # Sort distance data
+            dist_sorted = np.sort(distance, order='dist')
+                        
+            # Pick the first k distance values
+            k_dist = dist_sorted[0:self.k]
+            
+            sum = 0.0
+            for n in range(0,self.k):
+                sum += self.Ydata[ k_dist[n][0] ]
+            #end for
+            
+            Yout[i] = sum/self.k            
+        #end for
+        
+        return Yout
