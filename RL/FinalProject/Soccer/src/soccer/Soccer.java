b16c62246468ec7fb781fc29e3209f04ebb7df8a
Jacob Kilver <robobookworm@gmail.com>
2016-04-06 22:05:56 -0400

    RL Final project
    I can run and get numbers, but the game isn't what was shown in the paper and I am not sure that the values are entirely correct. At least I can get something to write about even if it isn't very good

--- RL/FinalProject/Soccer/src/soccer/Soccer.java
+++ RL/FinalProject/Soccer/src/soccer/Soccer.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package soccer;
+
 
 import java.io.*;
 import java.lang.Math;
@@ -29,10 +25,22 @@ import burlap.oomdp.stochasticgames.SGAgentType;
 import burlap.behavior.stochasticgames.madynamicprogramming.AgentQSourceMap.HashMapAgentQSourceMap;
 import burlap.behavior.stochasticgames.madynamicprogramming.QSourceForSingleAgent.HashBackedQSource;
 import burlap.oomdp.statehashing.SimpleHashableStateFactory;
+import burlap.oomdp.statehashing.HashableStateFactory;
 import burlap.behavior.valuefunction.ValueFunctionInitialization.ConstantValueFunctionInitialization;
 import burlap.behavior.stochasticgames.madynamicprogramming.backupOperators.MaxQ;
 import burlap.behavior.stochasticgames.madynamicprogramming.backupOperators.CoCoQ;
 import burlap.behavior.stochasticgames.madynamicprogramming.backupOperators.MinMaxQ;
+import burlap.oomdp.stochasticgames.World;
+import burlap.behavior.stochasticgames.madynamicprogramming.dpplanners.MAValueIteration;
+import burlap.behavior.stochasticgames.madynamicprogramming.policies.EGreedyMaxWellfare;
+import burlap.behavior.stochasticgames.agents.madp.MultiAgentDPPlanningAgent;
+import burlap.behavior.stochasticgames.agents.maql.MultiAgentQLearning;
+import burlap.behavior.stochasticgames.GameAnalysis;
+import burlap.behavior.stochasticgames.PolicyFromJointPolicy;
+import burlap.debugtools.DPrint;
+import burlap.behavior.stochasticgames.madynamicprogramming.*;
+import burlap.behavior.stochasticgames.madynamicprogramming.JAQValue;
+
 
 /**
  *
@@ -49,6 +57,15 @@ public class Soccer
     CorrelatedQ cq;
     String A = "agent0";
     String B = "agent1";
+    HashableStateFactory hashingFactory;
+    World w;
+    double discount = 0.9;
+    double learningRate = 0.001;
+    double epsilon = 0.1;
+    MultiAgentQLearning a0;
+    MultiAgentQLearning a1;
+    EGreedyMaxWellfare ja0;
+    EGreedyMaxWellfare ja1;
     
     // Might need to override standardMechanics to account for soccer ball
     public Soccer()
@@ -57,6 +74,9 @@ public class Soccer
         
         // Generate domain
         d = (SGDomain)gg.generateDomain();
+        
+        // Get hashing factory?
+        hashingFactory = new SimpleHashableStateFactory();
         
         // Generate a clean state
         // The walls must include the boundary walls
@@ -77,22 +97,42 @@ public class Soccer
         // Set up boundary walls    
         GridGame.setBoundaryWalls(s, 4, 2);
         
-        // For Corr Q learning
-        // Agent map
-        agentDefinitions = new HashMap<String, SGAgentType>();
-        agentDefinitions.put(A, GridGame.getStandardGridGameAgentType(d));
-        agentDefinitions.put(B, GridGame.getStandardGridGameAgentType(d));
+        // I think everything up until now is good
         
-        // Q value map
-        qSourceMapping = new HashMap<String, HashBackedQSource>();
-        qSourceMapping.put(A, new HashBackedQSource(new SimpleHashableStateFactory(), new ConstantValueFunctionInitialization()));
-        qSourceMapping.put(B, new HashBackedQSource(new SimpleHashableStateFactory(), new ConstantValueFunctionInitialization()));
+        HashMap goals = new HashMap<Integer, Double>();
+        goals.put(0, 100.0);
+        goals.put(1, 100.0);
+        JointReward rf = new GridGame.GGJointRewardFunction(d, 0, 0, false, goals);
+        TerminalFunction tf = new GridGame.GGTerminalFunction(d);
+        SGAgentType at = GridGame.getStandardGridGameAgentType(d);
         
-        qSourceMap = new HashMapAgentQSourceMap(qSourceMapping);
+        w = new World(d, rf, tf, s);
         
+        double defaultQ = 100;
+                
         // Correlated Q learner
         cq = new CorrelatedQ(CorrelatedEquilibriumSolver.CorrelatedEquilibriumObjective.UTILITARIAN);
         
+        a0 = new MultiAgentQLearning(d, discount, learningRate, hashingFactory, defaultQ, cq, true);
+        a1 = new MultiAgentQLearning(d, discount, learningRate, hashingFactory, defaultQ, cq, true);
+            
+        a0.joinWorld(w, at);
+        a1.joinWorld(w, at);
+        
+        // Define the policy
+        //set their policies to be a epsilon greedy maxwelfare (which CoCo-Q uses) policy over the joint actions
+        //with ties broken randomly
+        ja0 = new EGreedyMaxWellfare(a0, epsilon);
+        ja1 = new EGreedyMaxWellfare(a1, epsilon);
+        ja0.setBreakTiesRandomly(true);
+        ja1.setBreakTiesRandomly(true);
+
+        a0.setLearningPolicy(new PolicyFromJointPolicy(a0.getAgentName(), ja0));
+        a1.setLearningPolicy(new PolicyFromJointPolicy(a1.getAgentName(), ja1));
+        
+        
+        //don't have the world print out debug info (comment out if you want to see it!)
+        DPrint.toggleCode(w.getDebugId(), false);
         // How do I set up soccer ball dynamics?
         // I think I want to put it in the transition dynamics
         // Player B starts with ball always. If collision with Player A
@@ -103,22 +143,24 @@ public class Soccer
         // whether player has ball or not
     }
     
-    private double solveCorrQ()
+    private void playSoccer()
     {
-        double val = 0.0;
-        //val = cq.performBackup(s, A, agentDefinitions, qSourceMap);
+        // Take world and run game
+        int ngames = 1000;
+        for(int i = 0; i < ngames; i++)
+        {
+            GameAnalysis ga = w.runGame();
+            JointAction ja = w.getLastJointAction();
+            QSourceForSingleAgent q_a0 = a0.getMyQSource();
+            JAQValue jaq_a0 = q_a0.getQValueFor(s, ja);
+            System.out.println(jaq_a0.q);
+            //System.out.println(ga.maxTimeStep());
+        }
         
-        //MaxQ mq = new MaxQ();
-        //val = mq.performBackup(s, A, agentDefinitions, qSourceMap);
+        // Try using evaluateBehavior from EGreedyMaxWellfare for Q values....
         
-        //CoCoQ ccq = new CoCoQ();
-        //val = ccq.performBackup(s, A, agentDefinitions, qSourceMap);
-        
-        MinMaxQ mmq = new MinMaxQ();
-        val = mmq.performBackup(s, A, agentDefinitions, qSourceMap);
-        
-        return val;
     }
+    
 
     /**
      * @param args the command line arguments
@@ -129,15 +171,9 @@ public class Soccer
         
         // Learn game with Correlated Q
         
-        // Learn game with 
-        System.out.println("hello");
-        
-        //GridGame.main(args);
-        
         Soccer futbol = new Soccer();  
         
-        System.out.println(futbol.solveCorrQ());
-        System.out.println(futbol.solveCorrQ());
+        futbol.playSoccer();
     }
     
     
