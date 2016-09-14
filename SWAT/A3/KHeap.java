33fc4deaa16e16ce69d01916aef94a755638e6f2
Jacob Kilver <robobookworm@gmail.com>
2016-09-13 22:04:44 -0400

    SW Analysis and Test A3 part 1 K Heap for Korat
    I think these should work

--- SWAT/A3/KHeap.java
+++ SWAT/A3/KHeap.java
@@ -9,6 +9,54 @@ public class KHeap {
     public boolean repOK() {
         // returns true if shape and heap property are satisfied
         // complete this method
+        
+        // Make sure the array is not null
+        if (array == null)
+        {
+            return false;
+        }
+        
+        // Make sure that the size is reasonable
+        // Note: I am not sure why the check here is not to see if the
+        // size matches the size of the array
+        if (size < 0 || size > array.length)
+        {
+            return false;
+        }
+        
+        // Go through every non-empty element
+        for (int i = 0; i < size; i++)
+        {
+            // Ensure current array element is valid (non-empty)
+            int elem_i = array[i];
+            if (elem_i == -1)
+            {
+                return false;
+            }
+            
+            // Ensure that the ordering is correct
+            // The parent is greater than the child
+            if (i < 0)
+            {
+                int elem_parent = array[ (i-1)/k ];
+                if(elem_i < elem_parent)
+                {
+                    return false;
+                }
+            }
+        }
+        
+        // Check that the elements at the end of the array
+        // are empty
+        for (int i = size; i < array.length; i++)
+        {
+            if (array[i] != -1)
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 
     public static IFinitization finKHeap(int k, int maxSize, int maxArrayLength, int maxArrayValue) {
@@ -16,5 +64,16 @@ public class KHeap {
         IIntSet ks = f.createIntSet(k, k);
         f.set("k", ks);
         //complete the remaining method
+        // Define limits on size member
+        IIntSet sizes = f.createIntSet(0, 1, maxSize);
+        f.set("size", sizes);
+        
+        // Define limits on heap array
+        IIntSet arrayLength = f.createIntSet(0, 1, maxArrayLength);
+        IIntSet arrayValues = f.createIntSet(-1, 1, maxArrayValue);
+        IArraySet arrays = f.createArraySet(int[].class, arrayLength, arrayValues, 1);
+        f.set("array", arrays);
+        
+        return f;
     }
 }
