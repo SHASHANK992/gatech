4a9846379f6a955f3d0e252289cc6c947d57bd4b
cs6340 <robobookworm@gmail.com>
2016-09-21 20:49:25 -0400

    SWAT A3 Final Submission

--- SWAT/A3/AcyclicGraph.java
+++ SWAT/A3/AcyclicGraph.java
@@ -9,7 +9,7 @@ public class AcyclicGraph {
     }
 
     //************************
-    // 
+    // Determine if the test case is valid
     //************************
     public boolean repOK() {
         // returns true if and if only the graph reachable from "root" is a directed acyclic graph
@@ -34,58 +34,59 @@ public class AcyclicGraph {
         // - False means we are done traversing the subtree
         // - If the node is not present at all, that means it has never been visited
         Hashtable<Vertex, Boolean> visiting =  new Hashtable<Vertex, Boolean>();
-        boolean result = HasCycles(root, visiting); 
-        if(!result)
-        {
-            PrintGraph(root);
-        }     
+        boolean result = HasCycle(root, visiting);     
         
         return !result;               
     }
     
     // Returns true if the subgraph of node has a cycle
-    private boolean HasCycles(Vertex node, Hashtable<Vertex, Boolean> visiting)
-    {
-        // If the node is null, it can't have any children
-        // Can't be added to the dictionary, though. 
-        if(node == null)
+    private boolean HasCycle(Vertex node, Hashtable<Vertex, Boolean> visiting)
+    {                          
+        // If the current node isn't even in the dictionary,
+        // we know we haven't been here before
+        if (!visiting.containsKey(node))
         {
-            return false;
-        }
-        else
-        {
-            // If the current node isn't even in the dictionary,
-            // we know we haven't been here before
-            if (!visiting.containsKey(node))
-            {
-                // We are now in the process of traversing the subtree
-                // for this node
-                visiting.put(node, true);
+            // We are now in the process of traversing the subtree
+            // for this node
+            visiting.put(node, true);
+            
+            // In a normal DAG, there can be multiple links between the same nodes
+            // That is not the case for this assignment
+            // Need to make sure that none of the children are visited multiple times
+            HashSet<Vertex> visitedChildren = new HashSet<Vertex>();
+            
+            // Go through all the children of this node
+            for(int i = 0; i < node.outgoingEdges.length; i++)
+            {              
+                Vertex child = node.outgoingEdges[i];
                 
-                // Go through all the children of this node
-                for(int i = 0; i < node.outgoingEdges.length; i++)
-                {
-                    Vertex child = node.outgoingEdges[i];
-                    
+                // Add the child to the set of visited children   
+                if(visitedChildren.add(child))
+                {                       
                     // When we check the cycles of this subtree, if it
                     // returns true, we have found a cycle
                     // This needs to be reflected globally
-                    //System.out.println("Recursively calling HasCycles");
-                    if(HasCycles(child, visiting))
+                    if(HasCycle(child, visiting))
                     {
                         return true;
                     }
                 }
+                else
+                {
+                    // The child has been visited previously
+                    // A cycle might not exist, but we can't have more than
+                    // one edge between two nodes
+                    return true;
+                }
             }
-            else if(visiting.get(node))
-            {
-                // If in the process of traversing the subtrees we
-                // encounter a node we are still in the process
-                // of visiting, we have found a cycle
-                //System.out.println("Found cycle!!!");
-                return true;
-            }
-        }
+        } 
+        else if(visiting.get(node))
+        {
+            // If in the process of traversing the subtrees we
+            // encounter a node we are still in the process
+            // of visiting, we have found a cycle
+            return true; 
+        }       
         
         // Otherwise we have fully traversed the subtree
         // Mark the node as visited. Return false since
@@ -93,28 +94,7 @@ public class AcyclicGraph {
         visiting.put(node, false);
         return false;
     }
-    
-    private void PrintGraph(Vertex root)
-    {
-        System.out.println("Root: " + root);
-        for(int i = 0; i < root.outgoingEdges.length; i++)
-        {
-            Vertex child = root.outgoingEdges[i];
-            System.out.println("Child: " + child);
-            
-            if(child != null)
-            {
-                for(int j = 0; j < child.outgoingEdges.length; j++)
-                {
-                    Vertex grandchild = child.outgoingEdges[j];
-                    System.out.println("Grandchild: " + grandchild);
-                } 
-            }           
-        }
         
-        
-    }
-    
     /*
     Finitization method
     */
@@ -138,11 +118,11 @@ public class AcyclicGraph {
         {
             num = nodesNum-1;
         }
-        IIntSet arrayLength = f.createIntSet(0, num);
+        IIntSet arrayLength = f.createIntSet(0, nodesNum);
         
         // I need to exclude null from the remainder of the vertices
         // It causes duplication problems if I leave null there
-        vertices = f.createObjSet(Vertex.class, num, false);
+        vertices = f.createObjSet(Vertex.class, nodesNum, false);
         
         // The values the array can take is the set of vertices
         // I need to create nodesNum arrays because there needs to be one array for each node/vertex     
