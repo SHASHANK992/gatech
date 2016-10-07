d93e9bdf298a6ecfc666d1d6272dd8a9fa197ef9
cs6340 <robobookworm@gmail.com>
2016-10-06 22:06:03 -0400

    SWAT A5 more progress

--- SWAT/A5/dataflow/cs6340/src/LivenessAnalysis.java
+++ SWAT/A5/dataflow/cs6340/src/LivenessAnalysis.java
@@ -7,7 +7,9 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Iterator;
+import java.util.Map.Entry;
 
 import chord.project.analyses.JavaAnalysis;
 import chord.project.Chord;
@@ -17,6 +19,7 @@ import joeq.Class.jq_Method;
 import joeq.Compiler.Quad.ControlFlowGraph;
 import joeq.Compiler.Quad.BasicBlock;
 import joeq.Compiler.Quad.Quad;
+import joeq.Compiler.Quad.RegisterFactory;
 
 @Chord(name="liveness")
 public class LivenessAnalysis extends DataflowAnalysis<Register> {
@@ -62,9 +65,7 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 		
 		// Parse the control flow graph according to the rules for
 		// live variable analysis
-		boolean somethingChanged = false;
-		
-		somethingChanged = ParseCFG(exitBlock, inMap, outMap);
+		ParseCFG(liveCFG, inMap, outMap);
 		
 		/*	
 		do
@@ -79,43 +80,91 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	// out sets. The in and out sets are passed by reference so they are updated by
 	// default. This will return true if either set was updated during processing. 
 	// Otherwise it returns false.
-	private boolean ParseCFG(BasicBlock b, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out)
+	private void ParseCFG(ControlFlowGraph cfg, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out)
 	{
 	    // Live analysis works in reverse
 	    
-	    boolean somethingChanged = false;
+	    RegisterFactory rf = cfg.getRegisterFactory();
 	    
-	    // Work backwards through the quads in this block
-	    int numQuads = b.size();
-	    for(int index = numQuads-1; index >= 0; index--)
+	    //BasicBlock exitBlock = cfg.exit();
+	    List<BasicBlock> bList = cfg.reversePostOrder();
+	    
+	    boolean setsEqual = false;
+	    
+	    do
 	    {
-	        Quad q = b.getQuad(index);
-	        somethingChanged = somethingChanged || InUpdate(q, in);
-	        somethingChanged = somethingChanged || OutUpdate(q, out);
-	    }
+	        // Parse by block, which consists of a set of 
+	        // quads (which is what I really want)
+	        
+	        // Keep a copy of the in and out sets
+	        Map<Quad, Set<Register>> inPrime = DeepCopy(in);
+	        Map<Quad, Set<Register>> outPrime = DeepCopy(out);
+	        
+	        // For every block of the graph
+	        for(BasicBlock block : bList)
+	        {
+	            // Parse the quads in reverse order
+	            int numQuads = block.size();
+	            for(int index=numQuads-1; index >=0; index--)
+	            {
+	                Quad q = block.getQuad(index);
+	                
+	                // Update the IN set
+	                
+	                
+	                // The OUT set of the current quad is the union of all
+	                // the immediate successor quads' IN sets
+	                // Unless this is the last quad of the block, there can be only one successor
+	                // to this quad
+	                if(index == numQuads-1)
+	                {
+	                    // Find all the successor blocks
+	                    
+	                    // Get the first quads from all the successor blocks
+	                    
+	                    // Get the IN sets from all these successor quads
+	                    
+	                    
+	                }
+	                else
+	                {
+	                    // Get the quad for the sole immediate successor
+	                    Quad successor = block.getQuad(index+1);
+	                    
+	                    // Union In set of successor to the OUT set of the current quad
+	                    
+	                }
+	            }
+	        }	        	        
+	        
+	        // Compare the initial sets to the new sets to see if anything changed
+	        setsEqual = (in.equals(inPrime) && out.equals(outPrime));
+	    } while(!setsEqual);
 	    
-	    // Once all the quads have been checked, find the 
-	    // preceding blocks
-	    List<BasicBlock> preds = b.getPredecessors();
-	    
-	    // Parse these blocks just like you parsed the current block
-	    for(Iterator i = preds.iterator(); i.hasNext(); )
-	    {
-            BasicBlock block = (BasicBlock) i.next();
-   	        somethingChanged = somethingChanged || ParseCFG(block, in, out);
-	    }
-	    
-	    return somethingChanged;
 	}
 	
-	private boolean InUpdate(Quad q, Map<Quad, Set<Register>> in)
+	private void InUpdate(Quad q, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out)
 	{
-	    return false;
+	    // The in set of the given quad is (OUT[q] - DEF[q]) UNION USE[q]
 	}
 	
-	private boolean OutUpdate(Quad q, Map<Quad, Set<Register>> out)
+	private void OutUpdate(Quad q, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out)
 	{
-	    return false;
+	    
+	}
+	
+	
+	// Perform a deep copy for the in and out sets
+	public static Map<Quad, Set<Register>> DeepCopy( Map<Quad, Set<Register>> original )
+	{
+	    Map<Quad, Set<Register>> copy = new HashMap<Quad, Set<Register>>();
+	    
+	    for(Entry<Quad, Set<Register>> entry : original.entrySet())
+	    {
+	        copy.put(entry.getKey(), new HashSet<Register>(entry.getValue()));
+	    }
+	    
+	    return copy;
 	}
 }
 
