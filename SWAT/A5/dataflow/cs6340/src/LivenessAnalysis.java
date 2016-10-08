559ebce064be734be9c6256af0021d088b588635
cs6340 <robobookworm@gmail.com>
2016-10-08 17:50:17 -0400

    SWAT A5 Liveness passes, reaching def still needs work

--- SWAT/A5/dataflow/cs6340/src/LivenessAnalysis.java
+++ SWAT/A5/dataflow/cs6340/src/LivenessAnalysis.java
@@ -18,7 +18,6 @@ import joeq.Class.jq_Method;
 import joeq.Compiler.Quad.ControlFlowGraph;
 import joeq.Compiler.Quad.BasicBlock;
 import joeq.Compiler.Quad.Quad;
-//import joeq.Compiler.Quad.RegisterFactory;
 import joeq.Compiler.Quad.RegisterFactory.Register;
 import joeq.Compiler.Quad.Operand.RegisterOperand;
 
@@ -77,21 +76,21 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	// Otherwise it returns false.
 	private void ParseCFG(ControlFlowGraph cfg, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out)
 	{
-	    System.out.println("Parsing CFG");
+	    //System.out.println("Parsing CFG");
 	    
 	    // Live analysis works in reverse, but I am not sure how well I can leverage that here
 	    // Get list of blocks in CFG
 	    List<BasicBlock> bList = cfg.reversePostOrder();
 	    
 	    boolean setsEqual = false;
-	    System.out.println("Entering loop");
+	    ////System.out.println("Entering loop");
 	    do
 	    {
 	        // Parse by block, which consists of a set of 
 	        // quads (which is what I really want)
 	        
 	        // Keep a copy of the in and out sets
-	        System.out.println("Deep copying");
+	        ////System.out.println("Deep copying");
 	        Map<Quad, Set<Register>> inPrime = DeepCopy(in);
 	        Map<Quad, Set<Register>> outPrime = DeepCopy(out);
 	        
@@ -100,13 +99,14 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	        {
 	            // Parse the quads in reverse order
 	            int numQuads = block.size();
-	            System.out.println("Num quads in this block: " + numQuads);
+	            //System.out.println("Num quads in this block: " + numQuads);
 	            
 	            for(int index=numQuads-1; index >=0; index--)
-	            {
+	            {	                
 	                Quad q = block.getQuad(index);
 	                
-	                System.out.println("Updating In set");
+	                //System.out.println("Quad ID = " + q.getID());
+	                //System.out.println("Updating In set");
 	                // Update the IN set
 	                // Pass in the USE and DEF sets for the current quad
 	                // Get the register operands
@@ -119,18 +119,21 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	                
 	                for(RegisterOperand ro : usedRegOps)
 	                {
+	                    ////System.out.println("User register operand " + ro.toString());
+	                    //System.out.println("Used register " + ro.getRegister().toString());
 	                    usedRegisters.add(ro.getRegister());
 	                }
 	                
 	                for(RegisterOperand ro : defRegOps)
 	                {
+	                    //System.out.println("Defined register " + ro.getRegister().toString());
 	                    defRegisters.add(ro.getRegister());
 	                }   
 	                
 	                InUpdate(q, in, out, usedRegisters, defRegisters);
 	                
 	                
-	                System.out.println("Updating out set");
+	                //System.out.println("Updating out set");
 	                // The OUT set of the current quad is the union of all
 	                // the immediate successor quads' IN sets
 	                // Unless this is the last quad of the block, there can be only one successor
@@ -138,55 +141,73 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	                Set<Register> succInSet = new HashSet<Register>();
 	                if(index == numQuads-1)
 	                {
-	                    System.out.println("Last quad in block");
+	                    //System.out.println("Last quad in block");
 	                    // Find all the successor blocks
 	                    List<BasicBlock> succBlocks = block.getSuccessors();
 	                    
-	                    System.out.println("Got successors");
+	                    //System.out.println("Got successors");
 	                    
 	                    // Get the first quads from all the successor blocks
 	                    for(BasicBlock succBlock : succBlocks)
 	                    {
-	                        System.out.println("Going through successors");
+	                        //System.out.println("Going through successors");
 	                        
 	                        // Get the first quad if it exists
 	                        if(succBlock.size() > 0)
 	                        {
 	                            Quad firstQuad = succBlock.getQuad(0);
 	                            
-	                            // Get the IN set for this quad	                            
-	                            Set<Register> tempInSet = in.get(firstQuad);
-	                            if( tempInSet == null)
+	                            // Get the IN set for this quad	
+	                            //Set<Register> tempInSet = new HashSet<Register>();                            
+	                            //Set<Register> tempInSet = in.get(firstQuad);
+	                            if( in.get(firstQuad) != null)
 	                            {
-	                                tempInSet = new HashSet<Register>();
+	                                for(Register r : in.get(firstQuad))
+	                                {
+	                                    succInSet.add(r);
+	                                }
 	                            }
 	                            
 	                            // Add the contents of the In set to the running union
-	                            for(Register r : tempInSet)
-	                            {
-	                                succInSet.add(r);
-	                            }
+	                            //for(Register r : tempInSet)
+	                            //{
+	                            //    succInSet.add(r);
+	                            //}
 	                        }
 	                    }                    
 	                }
 	                else
 	                {
-	                    System.out.println("Not last quad in block");
+	                    //System.out.println("Not last quad in block");
 	                    
 	                    // Get the quad for the sole immediate successor
 	                    Quad successor = block.getQuad(index+1);
 	                    
+	                    //System.out.println("Successor Quad ID = " + successor.getID());
+	                    
 	                    // Get the in set
-	                   succInSet = in.get(q);             
+	                    if( in.get(successor) != null)
+	                    {
+	                        for(Register r : in.get(successor))
+	                        {
+	                            succInSet.add(r);
+	                        }
+	                    }
+	                   //succInSet = in.get(q);             
 	                }
 	                
+	                for(Register r : succInSet)
+	                {
+	                    //System.out.println("Successor In set register " + r.toString());
+	                }
+	                	                
 	                OutUpdate(q, in, out, succInSet);
 	            }
 	        }	        	        
 	        
 	        // Compare the initial sets to the new sets to see if anything changed
 	        setsEqual = (in.equals(inPrime) && out.equals(outPrime));
-	        System.out.println("Are sets equal: " + setsEqual);
+	        //System.out.println("Are sets equal: " + setsEqual);
 	    } while(!setsEqual);
 	    
 	}
@@ -200,14 +221,14 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	
 	private void InUpdate(Quad q, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out, List<Register> use, List<Register> def)
 	{
-	    System.out.println("Within In update method");
+	    //System.out.println("Within In update method");
 	    // The in set of the given quad is (OUT[q] - DEF[q]) UNION USE[q]
 	    
 	    // Find the difference between the OUT set and the DEF set
 	    // I am a bit worried about copying in Java. I will force a 
 	    // deep copy here
 	    Set<Register> op1 = new HashSet<Register>();
-	    System.out.println("Deep copy out set");
+	    //System.out.println("Deep copy out set");
 	    if(out.get(q) != null)
 	    {
 	        for(Register r : out.get(q))
@@ -222,25 +243,30 @@ public class LivenessAnalysis extends DataflowAnalysis<Register> {
 	    }
 	    */
 	    
-	    System.out.println("Remove defined variables");
+	    //System.out.println("Remove defined variables");
 	    for(Register r : def)
 	    {
 	        op1.remove(r);
 	    }
 	    
-	    System.out.println("Adding used variables");
+	    //System.out.println("Adding used variables");
 	    for(Register r : use)
 	    {
 	        op1.add(r);
 	    }
 	    
-	    System.out.println("Assigning...");
+	    //System.out.println("Assigning Quad ID " + q.getID() + " the following set: ");
+	    //for(Register r : op1)
+	    //{
+	    //    System.out.print("   " + r.toString());
+	    //}
+	    //System.out.print("\n");
 	    in.put(q, op1);
 	}
 	
 	private void OutUpdate(Quad q, Map<Quad, Set<Register>> in, Map<Quad, Set<Register>> out, Set<Register> succInSet)
 	{
-	    System.out.println("In OutUpdate method");
+	    //System.out.println("In OutUpdate method");
 	    out.put(q, succInSet);
 	}
 	
--- SWAT/A5/dataflow/cs6340/src/ReachDefAnalysis.java
+++ SWAT/A5/dataflow/cs6340/src/ReachDefAnalysis.java
@@ -1,7 +1,26 @@
 import chord.project.Chord;
 import chord.util.tuple.object.Pair;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import chord.project.analyses.JavaAnalysis;
+import chord.project.Chord;
+import chord.program.Program;
+
+import joeq.Class.jq_Method;
+import joeq.Compiler.Quad.ControlFlowGraph;
+import joeq.Compiler.Quad.BasicBlock;
 import joeq.Compiler.Quad.Quad;
 import joeq.Compiler.Quad.RegisterFactory.Register;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
 
 @Chord(name="reachdef")
 public class ReachDefAnalysis extends DataflowAnalysis<Pair<Quad, Register>> {
@@ -36,6 +55,175 @@ public class ReachDefAnalysis extends DataflowAnalysis<Pair<Quad, Register>> {
 		// Note: This is a single procedure analysis; you do not need to
 		// consult any pointer analysis, call graph, or any method of the given
 		// program besides the provided main method.
+		
+		// Reach definition is a forward, MAY analysis
+		// Begin with IN and OUT sets being empty
+		
+		ControlFlowGraph reachCFG = main.getCFG();
+		
+		// Get first Basic Block
+		// This block has no quads
+		// Note: This is currently unused, but may be used later
+		BasicBlock startBlock = reachCFG.entry();
+		
+		// Parse the control flow graph according to the rules for
+		// reaching definition analysis
+		ParseCFG(reachCFG, inMap, outMap);	
 	}
+	
+	private void ParseCFG(ControlFlowGraph cfg, Map<Quad, Set<Pair<Quad, Register>>> in, Map<Quad, Set<Pair<Quad,Register>>> out)
+	{
+	    // Reaching analysis works forward, but I am not sure how well I can leverage
+	    // that here
+	    // Get list of blocks in CFG
+	    List<BasicBlock> bList = cfg.reversePostOrder();
+	    
+	    boolean setsEqual = false;
+	    
+	    do
+	    {
+	        // Keep a copy of the in and out sets
+	        Map<Quad, Set<Pair<Quad, Register>>> inPrime = DeepCopy(in);
+	        Map<Quad, Set<Pair<Quad, Register>>> outPrime = DeepCopy(out);
+	        
+	        for(BasicBlock block : bList)
+	        {
+	            // Parse the quads in forward order
+	            int numQuads = block.size();
+	        
+	            for(int index=0; index < numQuads; index++)
+	            {	     
+	                Quad q = block.getQuad(index);
+	                           
+	                //********************************
+	                // Update the IN set
+	                //*********************************
+	                // The union of all the predecessor quads OUT sets
+	                Set<Pair<Quad, Register>> predOutSet = new HashSet<Pair<Quad, Register>>();
+	                if(index == 0)
+	                {
+	                    // If this is the first quad of the block, need to find
+	                    // the last quad of all the preceding blocks
+	                    List<BasicBlock> predBlocks = block.getPredecessors();
+	                    
+	                    for(BasicBlock predBlock : predBlocks)
+	                    {
+	                        // Get the last quad if it exists
+	                        if(predBlock.size() > 0)
+	                        {
+	                            Quad lastQuad = predBlock.getLastQuad();
+	                            
+	                            if(out.get(lastQuad) != null)
+	                            {
+	                                for(Pair<Quad, Register> p : out.get(lastQuad))
+	                                {
+	                                    predOutSet.add(p);
+	                                }
+	                            }
+	                        }
+	                    }	                    
+	                }
+	                else
+	                {
+	                    // If this is not the first quad, there is only
+	                    // one predecessor to this quad
+	                    Quad predecessor = block.getQuad(index-1);
+	                    
+	                    if(out.get(predecessor) != null)
+	                    {
+	                        predOutSet = out.get(predecessor);
+	                        /*
+	                        for(Register r : out.get(predecessor))
+	                        {
+	                            predOutSet.add(r);
+	                        }
+	                        */
+	                    }
+	                }
+	                
+	                InUpdate(q, in, out, predOutSet);
+	               
+	                //***************************************
+	                // Update the OUT set 
+	                //***************************************
+	                // Get the used and defined registers
+	                List<RegisterOperand> usedRegOps = q.getUsedRegisters();
+	                List<RegisterOperand> defRegOps = q.getDefinedRegisters();
+	                
+	                // Convert the register operands to registers
+	                List<Register> usedRegisters = new LinkedList<Register>();
+	                List<Register> defRegisters = new LinkedList<Register>();
+	                
+	                for(RegisterOperand ro : usedRegOps)
+	                {
+	                    usedRegisters.add(ro.getRegister());
+	                }
+	                
+	                for(RegisterOperand ro : defRegOps)
+	                {
+	                    defRegisters.add(ro.getRegister());
+	                }
+	                
+	                OutUpdate(q, in, out, usedRegisters, defRegisters);            
+	            }
+	        }	        
+	        
+	        // Compare sets from before to after
+	        setsEqual = (in.equals(inPrime) && out.equals(outPrime));	    
+	    } while(!setsEqual);
+	
+	}
+	
+	private void InUpdate(Quad q, Map<Quad, Set<Pair<Quad, Register>>> in, Map<Quad, Set<Pair<Quad,Register>>> out, Set<Pair<Quad,Register>> predOutSet)
+	{
+	    in.put(q, predOutSet);
+	}
+	
+	private void OutUpdate(Quad q, Map<Quad, Set<Pair<Quad, Register>>> in, Map<Quad, Set<Pair<Quad, Register>>> out, List<Register> use, List<Register> def)
+	{
+	    // My update seems a little weird. I am not sure if it is just the
+	    // way we have the problem set up (registers) instead of (register, quad) pairs
+	
+	    // Remove from the IN set all the <Quad, Register> Pairs that use registers defined by the DEF set
+	    Set<Pair<Quad, Register>> temp = in.get(q);
+	    if(temp == null)
+	    {
+	        temp = new HashSet<Pair<Quad, Register>>();
+	    }
+	    
+	    for(Register r : def)
+	    {
+	        for(Pair<Quad, Register> p : temp)
+	        {
+	            if(p.val1.equals(r))
+	            {
+	                temp.remove(p);
+	            }
+	        }	        
+	    }
+	    
+	    // Add in all the <Quad, Register> pairs for the registers defined
+	    for(Register r : def)
+	    {
+	        temp.add(new Pair<Quad, Register>(q,r));
+	    }
+	    
+	    out.put(q, temp);
+	}
+	
+	
+	// Perform a deep copy for the in and out sets
+	public static Map<Quad, Set<Pair<Quad,Register>>> DeepCopy( Map<Quad, Set<Pair<Quad,Register>>> original )
+	{
+	    Map<Quad, Set<Pair<Quad, Register>>> copy = new HashMap<Quad, Set<Pair<Quad, Register>>>();
+	    
+	    for(Entry<Quad, Set<Pair<Quad, Register>>> entry : original.entrySet())
+	    {
+	        copy.put(entry.getKey(), new HashSet<Pair<Quad, Register>>(entry.getValue()));
+	    }
+	    
+	    return copy;
+	}
+	
 }
 
